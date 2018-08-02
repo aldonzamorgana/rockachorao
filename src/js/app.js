8f0a7e2c41a5f1cccc41592b6c8cda6cd2dd0177
aldonzamorgana <avecesnoregresocuandoquiero@gmail.com>
2018-08-02 00:46:15 -0500

    slide espacios

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,14 @@ $(function(){
   var App = {};
   // Countries dropdown
   MainNav.init();
+  // CountriesDropdown.init()
   // B2bNav.init();
   // BuscadorEntidades.init();
   SlideFinancieras.init();
   Ticket.init();
+
+  App.countries_dropdown = new CountriesDropdown();
+  App.countries_dropdown.init();
+  
   // EnvioInstrucciones.init();
 });
